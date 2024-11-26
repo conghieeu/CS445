5bf073e8e1fda67c891b2d541588c68e238641f9
conghieeu <63394751+conghieeu@users.noreply.github.com>
2024-11-26 14:36:18 +0700

    GameOver, MoTa

--- Assets/_Data/Scripts/Core/Loader/SceneLoader.cs
+++ Assets/_Data/Scripts/Core/Loader/SceneLoader.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
+using System.Data.Common;
 using Core;
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
@@ -9,14 +11,26 @@ public class SceneLoader : GameBehavior
 {
     [Header("SCENES MANAGER")]
     [SerializeField] float transitionTime = 1f;
+    DataManager dataManager;
 
     public UnityAction OnStartLoadScene;
+
+    private void Start()
+    {
+        dataManager = DataManager.Instance;
+    }
+
+    public void LoadSceneMenuOnGameOver()
+    {
+        dataManager.OnStartNewGame();
+        LoadSceneMenu();
+    }
 
     public void LoadGameScene(GameScene gameScene)
     {
         Debug.Log($"Load scene {gameScene}");
         StartCoroutine(LoadSceneByGameScene(gameScene));
-    } 
+    }
 
     // delay để hiện ứng chuyển scene chạy
     IEnumerator LoadSceneByGameScene(GameScene gameScene)
